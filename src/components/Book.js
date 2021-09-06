a7b64e7baa4095cbe034d71d360fad2f2ca92264
julian3493 <julianramosarevalo@gmail.com>
2021-09-06 17:11:49 -0500

    create book component

---
+++ src/components/Book.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = ({ book }) => (
+  <tr>
+    <td>{book.id}</td>
+    <td>{book.title}</td>
+    <td>{book.category}</td>
+  </tr>
+);
+
+Book.propTypes = {
+  book: PropTypes.objectOf.isRequired,
+};
+
+export default Book;
