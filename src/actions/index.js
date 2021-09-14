95b9b1092d41deef66703016a1db43cb95c399de
julian3493 <julianramosarevalo@gmail.com>
2021-09-14 08:45:56 -0500

    create filter reducer and change_filter action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 export const CREATE_BOOK = 'CREATE_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
+export const CHANGE_FILTER = 'FILTER';
 
 export const createBook = (book) => ({ type: CREATE_BOOK, book });
 export const removeBook = (book) => ({ type: REMOVE_BOOK, book });
+export const filterBook = (filter) => ({ type: CHANGE_FILTER, filter})
